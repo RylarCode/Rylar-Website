44184ed5fb86d1b5cc720a5effbc73ffa4bd9a1c
RylarCode <rylarwong06@gmail.com>
2023-10-18 22:14:26 +0800

    Line now works but has bugs with line break

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 
 
 let calculatorDisplay = ""
+let currentLocation = 0
 
-function updateCurrentOperations(textToAdd, locationtoAdd){
-    calculatorDisplay += textToAdd
+function updateCurrentOperations(textToAdd){
+    // calculatorDisplay += textToAdd
+    let calculatorDisplayArray = calculatorDisplay.split('');
+    calculatorDisplayArray.splice(currentLocation, 0, textToAdd);
+    calculatorDisplay = calculatorDisplayArray.join('');
     displayCurrentOperations(calculatorDisplay)
+    moveCurrentLocation('right');
 }
 
 function displayCurrentOperations(calculatorDisplay){
     screenDisplay.textContent = calculatorDisplay
+
+    if (calculatorDisplay == "") {
+        screenDisplay.textContent = "0";
+    }
 }
 
 function calculateCurrentOperations(){
@@ -24,11 +33,40 @@ function calculateCurrentOperations(){
 function clearCurrentOperations(){
     calculatorDisplay = ""
     displayCurrentOperations(calculatorDisplay)
+    moveCurrentLocation('reset')
 }
 
 function deleteCurrentLocation(){
-    calculatorDisplay = calculatorDisplay.substring(0, calculatorDisplay.length-1)
+    // calculatorDisplay = calculatorDisplay.substring(0, calculatorDisplay.length-1)
+    let calculatorDisplayArray = calculatorDisplay.split('');
+    calculatorDisplayArray.splice(currentLocation-1, 1, '');
+    calculatorDisplay = calculatorDisplayArray.join('');
     displayCurrentOperations(calculatorDisplay)
+    moveCurrentLocation('left')
+}
+
+function moveCurrentLocation(direction){
+    // inputLine.style.marginLeft = '24px';
+    if (direction == 'left'){
+        currentLocation -= 1
+    } else if(direction == 'right'){
+        currentLocation += 1
+    } else if(direction == 'reset'){
+        currentLocation = 0
+    }
+
+
+    if (currentLocation < 0){
+        currentLocation = 0;
+    }
+
+    if (currentLocation > calculatorDisplay.length){
+        currentLocation = calculatorDisplay.length;
+    }
+
+
+    let currentLocationPx = String((currentLocation * 16.5) - 8) + 'px'
+    inputLine.style.marginLeft = currentLocationPx;
 }
 
 function buttonClicked(button){
@@ -36,10 +74,10 @@ function buttonClicked(button){
     
     switch (buttonText) {
         case '&lt;-':
-            console.log('<-');
+            moveCurrentLocation('left');
             break;
         case '-&gt;':
-            console.log('->');
+            moveCurrentLocation('right');
             break;
         case '=':
             calculateCurrentOperations();
@@ -51,7 +89,7 @@ function buttonClicked(button){
             deleteCurrentLocation()
             break;
         default:
-            updateCurrentOperations(buttonText,-1)
+            updateCurrentOperations(buttonText)
     }
 }
 
@@ -60,6 +98,7 @@ function buttonClicked(button){
 
 let buttons = document.getElementsByTagName("button")
 let screenDisplay = document.querySelector('.Calculator-Display-Main')
+let inputLine = document.querySelector('.Calculator-Display-Line')
 
 for (let i=0; i<buttons.length; i++){
     //Just writing the function calls it, but doesnt return it, in this case, you will have to define the function in the addeventlistener function
